db99e84e47249841a3e4164a65c4f1352eb44729
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-02 17:26:57 -0300

    Aula Template Literals

---
+++ javascript-3-js-moderno/notes.md
@@ -0,0 +1,2 @@
+# anotaçoes módulo
+
---
+++ javascript-3-js-moderno/templateLiterals/index.js
@@ -0,0 +1,11 @@
+const name = 'Wagner'
+
+console.log( `Olá, ${name}!
+Estamos em ${Date()}
+    2 + 2 = ${2 + 2}` );
+
+/*
+Olá, Wagner!
+Estamos em Wed Nov 02 2022 17:25:21 GMT-0300 (Horário Padrão de Brasília)
+    2 + 2 = 4
+*/
