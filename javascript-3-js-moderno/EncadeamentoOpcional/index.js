e06284bafb633c47c9a873ffb7d7a50b03b6fe52
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-03 10:55:05 -0300

    Aula de Encadeamento Opcional

---
+++ javascript-3-js-moderno/EncadeamentoOpcional/index.js
@@ -0,0 +1,30 @@
+//encadeamento opcional
+
+const user = {
+
+    name: 'John Doe',
+    email: 'doejohn@email.com',
+
+    friends: [{
+
+        name: 'Mary',
+
+        address: {
+            street: 'Some Street',
+            number: 89
+        }
+    }],
+
+    age: 42,
+
+    phone: {
+        countryCode: '+55',
+        ddd: '22',
+        number: '998765432'
+    }
+}
+
+// console.log( user.friends[0].phone.ddd ); // error
+console.log( user?.friends[0]?.phone?.ddd ); // undefined
+console.log( user?.brothers?.length ); // undefined
+console.log( user.brothers?.[5].name ); // undefined | ?. antes do array para ele conseguir fazer a validação
