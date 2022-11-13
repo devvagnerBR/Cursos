1043d0ffc3f0721ee3d156fef71ce90b26511e8e
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-13 10:41:34 -0300

    aula de sincrono

---
+++ Alura/JavaScript - consumindo e tratando dados de uma API/script.js
@@ -0,0 +1,12 @@
+console.log( `Mandando oi pro amigo` );
+
+
+const mandaMensagem = () => {
+    console.log( `tudo bem?` );
+    console.log( `Vou te mandar solicitação` );
+    console.log( `Solicitação recebida` );
+}
+
+mandaMensagem()
+
+console.log(`Tchau tchau`);
