1a2161d2dac2c4480e6b3cfd3eae362cc15fc903
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-14 20:37:19 -0300

    aula de renderização de array

--- Alura/react-escrevendo-com-typescript/src/App.tsx
+++ Alura/react-escrevendo-com-typescript/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Formulario from './components/Formulario';
+import Lista from './components/Lista';
 
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO WORLD</h1>
+      <Formulario />
+      <Lista />
     </div>
   );
 }
---
+++ Alura/react-escrevendo-com-typescript/src/components/Lista/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Lista = () => {
+
+    const tarefas = [{
+        tarefa: 'React',
+        tempo: '02:00:00'
+    }, {
+        tarefa: 'Javascript',
+        tempo: '01:30:00'
+    },
+    {
+        tarefa: 'Typescript',
+        tempo: '03:00:00'
+    }]
+
+    return (
+        <aside>
+            <h2>Estudos do dia</h2>
+            <ul>
+                {tarefas.map( ( tarefa, index ) => {
+                    return (
+                        <li key={index}>
+                            <h3>{tarefa.tarefa}</h3>
+                            <span>{tarefa.tempo}</span>
+                        </li>
+                    )
+                } )}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista
