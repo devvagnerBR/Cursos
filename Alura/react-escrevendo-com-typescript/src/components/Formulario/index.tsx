080e3df8822981a5adcfa195fec642877bace97c
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-15 14:48:48 -0300

    controlando formulário com Typescript

--- Alura/react-escrevendo-com-typescript/src/components/Formulario/index.tsx
+++ Alura/react-escrevendo-com-typescript/src/components/Formulario/index.tsx
@@ -3,9 +3,17 @@ import Botao from '../Botao'
 import style from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: '',
+        tempo: ''
+    }
+    AdicionarTarefa( event: React.FormEvent<HTMLFormElement> ) {
+        event.preventDefault()
+        console.log( `state: `, this.state );
+    }
     render() {
         return (
-            <form className={style.novaTarefa}>
+            <form onSubmit={this.AdicionarTarefa.bind( this )} className={style.novaTarefa}>
                 <div className={style.inputContainer}>
                     <label htmlFor='tarefa'>
                         Adicione um novo
@@ -13,6 +21,8 @@ class Formulario extends React.Component {
                     <input
                         type='text'
                         name='tarefa'
+                        value={this.state.tarefa}
+                        onChange={( e ) => this.setState( { ...this.state, tarefa: e.target.value } )}
                         id='tarefa'
                         placeholder='O que vê quer estudar'
                         required
@@ -23,11 +33,13 @@ class Formulario extends React.Component {
                     <label htmlFor='tempo'>
                         Tempo
                     </label>
-                    
+
                     <input
                         type='time'
                         step='1'
                         name='tempo'
+                        value={this.state.tempo}
+                        onChange={( e ) => this.setState( { ...this.state, tempo: e.target.value } )}
                         id='tempo'
                         min="00:00:00"
                         max="01:30:00"
--- Alura/react-escrevendo-com-typescript/src/components/Lista/index.tsx
+++ Alura/react-escrevendo-com-typescript/src/components/Lista/index.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
 import Item from './Item/index'
 import style from './Lista.module.scss'
 
 const Lista = () => {
 
-    const tarefas = [{
+    const [tarefas, setTarefas] = React.useState( [{
         tarefa: 'React',
         tempo: '02:00:00'
     }, {
@@ -13,11 +14,14 @@ const Lista = () => {
     {
         tarefa: 'Typescript',
         tempo: '03:00:00'
-    }]
+    }] )
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                console.log( `H2 CLICADO:`, tarefas );
+                setTarefas( [...tarefas, { tarefa: 'Estudar Estado', tempo: '05:00:00' }] )
+            }}>Estudos do dia</h2>
             <ul>
                 {tarefas.map( ( item, index ) => (
                     <Item key={index} {...item} />
