10e72e0976808a315f4a710d498e19fa3538776c
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-16 16:03:44 -0300

    Aula de string number e boolean

---
+++ Origamid/typescript-para-iniciantes/String-Number-Boolean/script.js
@@ -0,0 +1,14 @@
+"use strict";
+// string | number | boolean
+const frase = "Front-end";
+const preco = 500;
+const condi = true;
+// typeof
+//typeguard
+if (typeof frase === "string") {
+    console.log(`Frase é uma string`);
+}
+else {
+    console.log(`Frase não é uma string`);
+}
+console.log(typeof null); // object;
---
+++ Origamid/typescript-para-iniciantes/String-Number-Boolean/script.ts
@@ -0,0 +1,17 @@
+// string | number | boolean
+
+const frase: string = "Front-end";
+const preco: number = 500;
+const condi: boolean = true;
+
+// typeof
+//typeguard
+
+if ( typeof frase === "string" ) {
+    console.log( `Frase é uma string` );
+} else {
+    console.log( `Frase não é uma string` );
+}
+
+console.log( typeof null ) // object;
+
