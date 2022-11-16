6773400b21c9670aec239ecb5fd97ffae5914ff7
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-16 17:08:32 -0300

    exercicio 2

---
+++ Origamid/typescript-para-iniciantes/Exercicio2/script.js
@@ -0,0 +1,21 @@
+"use strict";
+// Minha Resolução
+function toNumber(value) {
+    if (typeof value === 'number')
+        console.log(typeof value, value);
+    else if (typeof value === 'string')
+        console.log(typeof value, Number(value));
+    else
+        console.log(`Value deve ser um número ou uma string`);
+}
+toNumber('21');
+// Origamid Resolução
+function toNumber2(value) {
+    if (typeof value === 'number')
+        return value;
+    else if (typeof value === 'string')
+        return Number(value);
+    else
+        throw 'Value deve ser um number | string';
+}
+console.log(toNumber2(200));
