ffc6d8890a5bc95c9fa04887efe26d1cb1741f77
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-11-17 17:25:45 -0300

    interface de DOM 2

---
+++ Origamid/typescript-para-iniciantes/Objetos-Interfaces/Interface-DOM/script.ts
@@ -0,0 +1,23 @@
+const video = document.querySelector( '#videoprincipal' )
+
+if ( video instanceof HTMLVideoElement ) {
+    console.log( video.volume )
+}
+
+const links = document.querySelectorAll( '.link' )
+console.log( links instanceof NodeList ); // true
+
+links.forEach( ( link ) => {
+    if ( link instanceof HTMLAnchorElement ) console.log( link );
+    else typeof link
+} )
+
+// Nodelist(QuerySelectorAll) - não possúi filter 
+// Array -  
+
+const arrayLinks = Array.from( links ) // transformando Node List em Array
+const anchorLinks = arrayLinks.filter(
+    ( link ) => link instanceof HTMLAnchorElement
+)
+
+console.log( anchorLinks );
