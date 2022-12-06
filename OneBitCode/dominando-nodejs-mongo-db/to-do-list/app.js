989bfc18bc8fc4835f91ac094ac2ae004befd9ab
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-06 09:36:22 -0300

    concentrando rotas

--- OneBitCode/dominando-nodejs-mongo-db/to-do-list/app.js
+++ OneBitCode/dominando-nodejs-mongo-db/to-do-list/app.js
@@ -1,38 +1,20 @@
 const express = require( 'express' )
+const checkListRouter = require( './src/routes/checklist' )
+
+
 
 const app = express()
-
-// requisição, resposta, proximo middleware
-const log = ( req, res, next ) => {
-
-    // mostrar o que veio no body da chamada
-    console.log( req.body );
-
-    console.log( `Date: ${Date.now()}` );
-    
-    // executa o proximo middleware
-    next()
-
-}
-
-// para o app conseguir usar o log
-app.use( log )
-
-// requisição get
-app.get( '/', ( req, res ) => {
-    res.send( '<h1>Minha Lista de tarefas ❤️</h1>' )
-} )
 
 // para enviar informações tipo json
 app.use( express.json() )
 
+app.use( '/checklists', checkListRouter )
 
-// requisição get
-app.get( '/json', ( req, res ) => {
-    console.log( ( req.body ) );
-    res.json( { title: 'Tarefa X', done: true } )
-} )
+
+
+
 
 app.listen( 3000, () => {
     console.log( 'server is running at 3000' );
 } )
+
---
+++ OneBitCode/dominando-nodejs-mongo-db/to-do-list/appAntigo.js
@@ -0,0 +1,39 @@
+const express = require( 'express' )
+
+const app = express()
+
+// requisição, resposta, proximo middleware
+const log = ( req, res, next ) => {
+
+    // mostrar o que veio no body da chamada
+    console.log( req.body );
+
+    console.log( `Date: ${Date.now()}` );
+
+    // executa o proximo middleware
+    next()
+
+}
+
+// para o app conseguir usar o log
+app.use( log )
+
+// requisição get
+app.get( '/', ( req, res ) => {
+    res.send( '<h1>Minha Lista de tarefas ❤️</h1>' )
+} )
+
+// para enviar informações tipo json
+app.use( express.json() )
+
+
+// requisição get
+app.get( '/json', ( req, res ) => {
+    console.log( ( req.body ) );
+    res.json( { title: 'Tarefa X', done: true } )
+} )
+
+app.listen( 3000, () => {
+    console.log( 'server is running at 3000' );
+} )
+
