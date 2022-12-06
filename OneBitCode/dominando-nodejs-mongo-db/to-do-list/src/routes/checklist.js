ccc51a74a316a871588565a2eaa40aa7f23d65b9
Wagner Guimarães <wagnerluizsg@outlook.com.br>
2022-12-06 09:45:40 -0300

    parametros nas rotas

--- OneBitCode/dominando-nodejs-mongo-db/to-do-list/src/routes/checklist.js
+++ OneBitCode/dominando-nodejs-mongo-db/to-do-list/src/routes/checklist.js
@@ -2,11 +2,29 @@ const express = require( 'express' )
 
 const router = express.Router()
 
-
+// rota GET
 router.get( '/', ( req, res ) => {
     console.log( 'Olá' );
-    res.send()
+    res.send() // enviando a requisição
 } )
 
 
-module.exports = router
+
+//Rota POST 
+router.post( '/', ( req, res ) => {
+    console.log( req.body ); // imprime o body no terminal
+    res.status( 200 ).json( req.body ) // retorna o status e o body para o usuário
+} )
+
+router.get( '/:id', ( req, res ) => {
+
+    console.log( req.params.id );
+    // pega a informação que veio no :id,
+    // (0bs:  se fosse :product teria que ser
+    //req.params.product)
+
+    res.send( `ID: ${req.params.id}` )
+
+} )
+
+module.exports = router // exportando as rotas
